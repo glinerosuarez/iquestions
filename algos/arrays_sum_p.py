842da46aacabc0c3f0c1a1450b2e9ff88afd801c
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-03-26 20:29:25 -0500

    hashmap solution

--- algos/arrays_sum_p.py
+++ algos/arrays_sum_p.py
@@ -57,7 +57,32 @@ def cumsum_nospace_solution(a: list[int], k: int) -> int:
     return count
 
 
+def hashmap_solution(a: list[int], k: int) -> int:
+    count = sum = 0
+    n = len(a)
+    (map := dict())[0] = 1
+    print(f"\narray: {a} k: {k}\n")
+    for i in range(n):
+        print(f"beginning of iteration {i}")
+        print(f"values in hashmap {map}")
+        print(f"Sum: {sum} and value is: {a[i]}")
+        sum += a[i]
+        print(f"Sum is now equal to {sum}")
+        print(f"Sum - k is equal to {sum - k}")
+        if sum - k in map:
+            print(f"Sum - k is already in hashmap so we increment the count map[sum - k] times: {map[sum - k]}")
+            count += map[sum - k]
+        else:
+            print(f"Sum - k: {sum - k} is not in hashmap we don't increment count in this iteration")
+        print(f"Storing sum in hashmap")
+        map[sum] = map.get(sum, 0) + 1
+        print(f"Hashmap is now {map}")
+
+    return count
+
+
 if __name__ == '__main__':
-    assert cumsum_nospace_solution([10, 2, -2, -20, 10], -10) == 3
-    assert cumsum_nospace_solution([9, 4, 20, 3, 10, 5], 33) == 2
-    assert cumsum_nospace_solution([1, 1, 1], 2) == 2
+    assert hashmap_solution([10, 2, -2, -20, 10], -10) == 3
+    assert hashmap_solution([9, 4, 20, 3, 10, 5], 33) == 2
+    assert hashmap_solution([1, 1, 1], 2) == 2
+    assert hashmap_solution([3, 4, 7, 2, -3, 1, 4, 2], 7) == 4
